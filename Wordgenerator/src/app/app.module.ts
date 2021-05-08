b1da8135c3f465e4a62cd3c73d94693061ed9265
indumen <alo.indumen@gmail.com>
2021-05-08 14:05:42 +0300

    modified:   ../Wordgenerator/src/app/app-routing.module.ts 	modified:   ../Wordgenerator/src/app/app.module.ts modified:   package-lock.json 	modified:   package.json modified:   src/app/app.component.html 	modified:   src/app/word-selection-page/word-selection-page.component.html

--- Wordgenerator/src/app/app.module.ts
+++ Wordgenerator/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { AddItemComponent } from './add-item/add-item.component';
+import { WordGeneratorComponent } from './word-generator/word-generator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     TodoItemComponent,
-    AddItemComponent
+    AddItemComponent,
+    WordGeneratorComponent
   ],
   imports: [
     BrowserModule,
