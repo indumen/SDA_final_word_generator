457e9c02718b7d8ebe0c8a79e02487e556ea3026
Stepan Bolotnikov <stepan@snackable.ai>
2021-05-09 12:49:29 +0300

    Get a list of words

--- word-generator/src/app/word-selection-page/word-selection-page.component.ts
+++ word-generator/src/app/word-selection-page/word-selection-page.component.ts
@@ -28,8 +28,29 @@ export class WordSelectionPageComponent implements OnInit {
     let newWords = '';
     const howMany = Number(this.wordNumber.value);
 
+    if (isNaN(howMany)) {
+      // isNaN("asdasd") -> true, "this is not a number"
+      // isNaN(123) -> false, "this is a number"
+      alert('This is not a number');
+      return; // stop this function
+    }
+
+    if (howMany > this.verbList.length) {
+      alert('We do not have that many words');
+
+      return;
+    }
+
+    const copyOfVerbs = [...this.verbList];
+
     for (let i = 0; i < howMany; i++) {
-      newWords += ' ' + this.verbList[Math.floor(Math.random() * this.verbList.length)]
+      const randomWordIndex = Math.floor(Math.random() * copyOfVerbs.length);
+      const [ourNewVerb] = copyOfVerbs.splice(
+        randomWordIndex, // kust me hakkame asju võtme arrays
+        1, // mitu elementi me võtame
+      );
+
+      newWords += ' ' + ourNewVerb;
     }
 
     this.verb = newWords;
